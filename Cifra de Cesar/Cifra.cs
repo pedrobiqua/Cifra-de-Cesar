f9572741f55fb1d1de9c696a86fff98366b1d652
Pedro <pedrobiqua@gmail.com>
2022-06-07 21:58:23 -0300

    first commit

---
+++ Cifra de Cesar/Cifra.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cifra_de_Cesar
+{
+    public class Cifra
+    {
+
+        public static String abc = "abcdefghijklmnopqrstuvwxyz";
+        
+
+        public static String Criptografar(String text, int chave)
+        {
+            String new_text = "";
+            text = text.ToLower();
+            foreach (var i in text)
+            {
+                for (int j = 0; j < abc.Length; j++)
+                {
+                    if (i.Equals(abc[j]))
+                    {
+                        if ((j + chave) > abc.Length)
+                        {
+                            new_text += abc[j - abc.Length + chave];
+                        }
+                        else
+                        {
+                            new_text += abc[j + chave];
+                            new_text = new_text.ToLower();
+                        }
+                    }
+                }
+            }
+
+            return new_text;
+        }
+
+        public static String DesCriptografar(String text, int chave)
+        {
+            String new_text = "";
+            text = text.ToLower();
+            foreach (var i in text)
+            {
+                for (int j = 0; j < abc.Length; j++)
+                {
+                    if (i.Equals(abc[j]))
+                    {
+                        if ((j - chave) < 0)
+                        {
+                            new_text += abc[(j + abc.Length) - chave];
+                        }
+                        else
+                        {
+                            new_text += abc[j - chave];
+                            new_text = new_text.ToLower();
+                        }
+                    }
+                }
+            }
+
+            return new_text;
+        }
+
+        public static int EncontrarChave(string chave)
+        {
+            for (int i = 0; i < abc.Length; i++)
+            {
+                if (abc[i] == chave[0])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ Cifra de Cesar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cifra_de_Cesar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TDE Matemática discreta");
+
+            Console.WriteLine("Frase a ser Convertida:");
+            var texto = Console.ReadLine();
+
+            Console.WriteLine("Chave:");
+            var chave = Console.ReadLine();
+
+            int novaChave = Cifra.EncontrarChave(chave);
+
+            if (novaChave == -1)
+            {
+                Console.WriteLine("Não foi possivel usar essa chave");
+            }
+
+            Console.WriteLine(Cifra.Criptografar(texto, novaChave));
+
+            Console.WriteLine("Criptografia:");
+            var textoDes = Console.ReadLine();
+
+            Console.WriteLine("Chave:");
+            var chaveDes = Console.ReadLine();
+
+            int novaChavedes = Cifra.EncontrarChave(chaveDes);
+
+            Console.WriteLine(Cifra.DesCriptografar(textoDes, novaChavedes));
+
+            Console.ReadLine();
+        }
+    }
+}
